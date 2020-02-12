a7462a8f82c65bdf1bff7bedfd677d7f4b682154
BricksParts <bricksparts@gmail.com>
2020-02-12 01:35:11 -0500

    Player controller update

--- Top Down Movement Prototype/Player.gd
+++ Top Down Movement Prototype/Player.gd
@@ -4,7 +4,7 @@ onready var pointer:= $Pointer
 onready var ray:= $Pointer/RayCast2D
 onready var reticle:= $Pointer/Reticle
 
-enum moveState {stopped, halt, run, glide, sprint, walled, falling, sidestep}
+enum moveState {stopped, walk, run, glide, sprint, walled, falling, sidestep}
 enum moveAction {none, jump, sidestep, roll, vault}
 
 enum otherAnimationStates {jumpCharged}
@@ -15,16 +15,21 @@ var playerQueuedMoveAction = moveAction.none
 
 const SPRINT_THRESHOLD = 1200
 
-const FORCE_RUN = 1600
+const FORCE_WALK = 1600
+const FORCE_RUN = 400
+const FORCE_RUN_TO_WALK = 500
 const FORCE_SPRINT = 1000
 const FORCE_SPRINT_DECEL = 1100
-const FORCE_HALT = 5
 const FORCE_GLIDE = 600
 const FORCE_GLIDE_HALT = 5
 const FORCE_JUMP = 200
 const FORCE_JUMP_STOP = 50
 const FORCE_SIDESTEP = 300
 
+const FORCE_WALK_DECEL = 8
+const FORCE_RUN_DECEL = 3
+
+const THRESHOLD_WALK = 110
 const THRESHOLD_RUN = 160
 const THRESHOLD_GLIDE = 50
 
@@ -36,6 +41,7 @@ const TIMER_JUMP_CHARGE_UP = 40
 const TICKS_PER_SECOND = 72
 const CLAMP_FUDGE = 5
 
+var isHoldingRun = false
 var isInputAxisLocked = false
 
 var jumpChargeUpTimer = 0
@@ -57,12 +63,22 @@ func _ready():
 func _physics_process(delta):
 	update_timers(delta)
 	update_input_axis()
+	update_is_holding_run()
 	update_player_state()
 	update_input_move_action()
 	apply_movement(delta)
 	velocity = move_and_slide(velocity)
 	update_animation()
+	print(playerMoveState)
 #	update_reticle()
+
+func update_is_holding_run():
+	if Input.is_action_pressed("action_roll"):
+		isHoldingRun = true
+#		print("Player is holding run")
+	else:
+		isHoldingRun = false
+#		print("not holding run")
 
 func update_input_axis():
 	if isInputAxisLocked == false:
@@ -72,7 +88,6 @@ func update_input_axis():
 
 func update_input_move_action():
 	if Input.is_action_just_pressed("action_jump"):
-		print("HELLO")
 		if playerMoveState == moveState.run or moveState.sprint:
 			playerQueuedMoveAction = moveAction.sidestep
 			jumpChargeUpTimer = TIMER_JUMP_CHARGE_UP
@@ -111,29 +126,51 @@ func update_player_state():
 	if playerMoveState == moveState.stopped:
 		#Stopped state
 		if inputAxis != Vector2.ZERO:
+			#transition from Stopped to Walk
+			playerMoveState = moveState.walk
+		
+		if isHoldingRun:
 			#transition from Stopped to Run
 			playerMoveState = moveState.run
 	
-	elif playerMoveState == moveState.halt:
-		#Halt state
-		if inputAxis != Vector2.ZERO:
-			#transition from Halt to Run
-			playerMoveState = moveState.run
-		elif velocity.length() == 0:
-			#transition from Halt to Stopped
+#	elif playerMoveState == moveState.halt:
+#		#Halt state
+#		if inputAxis != Vector2.ZERO:
+#			if isHoldingRun:
+#				#transition from Halt to Run
+#				playerMoveState = moveState.run
+#			else:
+#				#transition from Halt to Walk
+#				playerMoveState = moveState.walk
+#		elif velocity.length() == 0:
+#			#transition from Halt to Stopped
+#			playerMoveState = moveState.stopped
+	
+	elif playerMoveState == moveState.walk:
+		#Walk state
+		if inputAxis == Vector2.ZERO and velocity == Vector2.ZERO:
+			#transition from Walk to Stopped
 			playerMoveState = moveState.stopped
+		
+		if isHoldingRun:
+			playerMoveState = moveState.run
 	
 	elif playerMoveState == moveState.run:
 		#Run state
-		if playerInputMoveAction == moveAction.jump:
-			#initiate Jump from Run
-			do_action_jump()
-		elif playerInputMoveAction == moveAction.sidestep:
-			#initiate Sidestep from Run
-			do_action_sidestep()
-		elif inputAxis == Vector2.ZERO:
-			#transition from Run to Halt
-			playerMoveState = moveState.halt
+		if isHoldingRun == false:
+			if velocity == Vector2.ZERO:
+				#transition from Run to Stopped
+				playerMoveState = moveState.stopped
+			elif velocity.length() <= (THRESHOLD_WALK + CLAMP_FUDGE):
+				#transition from Run to Walk
+				playerMoveState = moveState.walk
+		
+#		if playerInputMoveAction == moveAction.jump:
+#			#initiate Jump from Run
+#			do_action_jump()
+#		elif playerInputMoveAction == moveAction.sidestep:
+#			#initiate Sidestep from Run
+#			do_action_sidestep()
 	
 	elif playerMoveState == moveState.sprint:
 		#Sprint state
@@ -176,14 +213,17 @@ func do_action_sidestep():
 
 func convert_grounded_speed_to_state():
 	if velocity.length() >= (THRESHOLD_RUN + CLAMP_FUDGE):
-		#If fast enough, transitions the player to sprinting
+		#if at the correct speed, transitions the player to Sprint
 		playerMoveState = moveState.sprint
-	elif inputAxis != Vector2.ZERO:
-		#If not fast enough to Sprint, checks if the player transitions to a run
+	elif velocity.length() >= (THRESHOLD_WALK + CLAMP_FUDGE):
+		#if at the correct speed, transitions the player to Run
 		playerMoveState = moveState.run
+	elif velocity.length() > 0:
+		#if at the correct speed, transitions the player to Walk
+		playerMoveState = moveState.walk
 	else:
-		#If not fast enough to Sprint, checks if the player transitions to a halt
-		playerMoveState = moveState.halt
+		#if at the correct speed, transitions the player to Stopped
+		playerMoveState = moveState.stopped
 
 func touch_ground():
 	#Slows the player by FORCE_JUMP_STOP
@@ -195,35 +235,50 @@ func sidestep_end():
 	velocity = velocityMemory
 	convert_grounded_speed_to_state()
 
+func calc_force(forceType, multiplier = 1):
+	#Calculates vector2 that represents the force of the given force type
+	return inputAxis * forceType * multiplier
+
 func calc_force_jump(multiplier = 1):
-	#Calculates a vector2 that represents the force from the starting phase of a jump
+	#calculates vector2 that represents the force from the starting phase of a jump
 	return inputAxis * FORCE_JUMP * multiplier
 
 func calc_force_sidestep(multiplier = 1):
-	#Calculates a vector2 that represents the force from the starting phase of a sidestep
+	#calculates vector2 that represents the force from the starting phase of a sidestep
 	return inputAxis * FORCE_SIDESTEP * multiplier
 
+func calc_force_walk(multiplier = 1):
+	#calculates vector2 that represents the 'input' force while in the Walk state
+	return inputAxis * FORCE_WALK * multiplier
+
+func calc_force_run_to_walk(multiplier = 1):
+	return inputAxis * FORCE_RUN_TO_WALK * multiplier
+
 func calc_force_run(multiplier = 1):
-	#Calculates a vector2 that represents the 'input' force from running
+	#calculates vector2 that represents the 'input' force while in the Run state
 	return inputAxis * FORCE_RUN * multiplier
 
-func calc_force_halt(multiplier = 1):
-	#Calculates a vector2 that represents the force while the player is sliding to a halt on the ground while there is no player input
-	return -velocity.normalized() * FORCE_HALT * multiplier * velocity.length()
-
 func calc_force_sprint(multiplier = 1):
-	#Calculates a vector2 that represents the 'acceleration' force a player inputs while sprinting
+	#calculates vector2 that represents the 'input' force while Sprint state
 	return inputAxis * FORCE_SPRINT * multiplier
 
+func calc_force_walk_decel(multiplier = 1):
+	#calculates vector2 that represents the 'friction' force while the player is slowing to Stopped state
+	return -velocity.normalized() * FORCE_WALK_DECEL * multiplier * velocity.length()
+
+func calc_force_run_decel(multiplier = 1):
+	#calculates vector2 that represents the 'friction' force while the player is slowing to Walk state
+	return -velocity.normalized() * FORCE_RUN_DECEL * multiplier * velocity.length()
+
 func calc_force_sprint_decel(multiplier = 1):
-	#Calculates a vector2 that represent the 'friction' force that slows a player down to the running threshold while sprinting
+	#calculates a vector2 that represent the 'friction' force that slows a player down to the running threshold while sprinting
 	return -velocity.normalized() * FORCE_SPRINT_DECEL * multiplier
 
 func calc_force_roll(vectorRoll):
 	pass
 
 func calc_force_glide(multiplier = 1):
-	#Calculates a vector2 that represents the 'input' force from gliding
+	#calculates a vector2 that represents the 'input' force from gliding
 	if inputAxis == Vector2.ZERO:
 		glideMotion = -glideMotion.normalized() * FORCE_GLIDE_HALT * multiplier * glideMotion.length()
 	else:
@@ -233,22 +288,41 @@ func calc_force_glide(multiplier = 1):
 
 func apply_movement(delta):
 #	var vectorMovementSum = Vector2()
-	if playerMoveState == moveState.halt:
-		if velocity.length() < CLAMP_FUDGE:
-			velocity = Vector2.ZERO
+	if playerMoveState == moveState.walk:
+		if inputAxis != Vector2.ZERO:
+			#player is inputting direction
+			velocity += calc_force_walk() * delta
+			velocity = velocity.clamped(THRESHOLD_WALK)
 		else:
-			velocity += calc_force_halt() * delta
+			#player is not inputting direction (halting)
+			if velocity.length() < CLAMP_FUDGE:
+				velocity = Vector2.ZERO
+			else:
+				velocity += calc_force_walk_decel() * delta
 	
 	elif playerMoveState == moveState.run:
-		velocity += calc_force_run() * delta
-		velocity = velocity.clamped(THRESHOLD_RUN)
+		if isHoldingRun:
+			if inputAxis != Vector2.ZERO:
+				#continue running
+				velocity += calc_force_run() * delta
+				velocity = velocity.clamped(THRESHOLD_RUN)
+			else:
+				velocity += calc_force_run_decel() * delta
+		else:
+			if inputAxis != Vector2.ZERO:
+				#continue running but slow to zero
+				velocity += calc_force_run_to_walk() * delta
+				velocity += calc_force_run_decel() * delta
+			else:
+				#transition to walking
+				velocity += calc_force_run_decel() * delta
 	
 	elif playerMoveState == moveState.sprint:
 		velocity += calc_force_sprint() * delta
 		velocity += calc_force_sprint_decel() * delta
 
 func update_animation():
-	if playerMoveState == moveState.halt:
+	if playerMoveState == moveState.stopped:
 		$AnimatedSprite.play("Halt")
 	elif playerMoveState == moveState.run:
 		$AnimatedSprite.play("Run")
